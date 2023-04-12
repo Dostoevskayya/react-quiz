616b6ca239fbd2875d49d02904a7f51107007fd1
Dostoevskayya <dost.alina@mail.ru>
2023-04-12 13:33:53 +0300

    add link

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function Result({ correct }) {
       <h2>
         Вы отгадали {correct} ответа из {questions.length}
       </h2>
-      <a href="/">
+      <a href="https://dostoevskayya.github.io/react-quiz/">
         <button>Попробовать снова</button>
       </a>
     </div>
